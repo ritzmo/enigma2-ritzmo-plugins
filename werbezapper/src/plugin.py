492ba07cb6a07a3f5c6a579a9b2def7600d8192c
ritzmo <ritzmo@a295cab0-163a-0410-857a-8207b47ee9d6>
2007-09-26 16:18:09 +0000

    WerbeZapper: Some more Comments and formatting fixes.
    git-svn-id: svn://svn.freaque.net/enigma2@77 a295cab0-163a-0410-857a-8207b47ee9d6

--- werbezapper/src/plugin.py
+++ werbezapper/src/plugin.py
@@ -1,9 +1,17 @@
+# GUI (Screens)
 from Screens.MessageBox import MessageBox
 from Screens.ChoiceBox import ChoiceBox
-from Plugins.Plugin import PluginDescriptor
+
+# Timer
 from enigma import eTimer
 
+# Plugin
+from Plugins.Plugin import PluginDescriptor
+
 class WerbeZapper:
+	"""Simple Plugin to automatically zap back to a Service after a given amount
+	   of time."""
+
 	def __init__(self, session, servicelist):
 		# Save Session&Servicelist
 		self.session = session
@@ -18,12 +26,28 @@ class WerbeZapper:
 		self.move_service = None
 
 	def showSelection(self):
+		# Check if timer is active
 		if self.zap_timer.isActive():
-			# Disable Timer?
-			self.session.openWithCallback(self.confirmStop, MessageBox, "Timer already running.\nStop it?")
+			# Ask if we should stop the running timer
+			self.session.openWithCallback(
+				self.confirmStop,
+				MessageBox,
+				"Timer already running.\nStop it?"
+			)
 		else:
 			# Select Timer Length
-			self.session.openWithCallback(self.conditionalConfirm, ChoiceBox, "When to Zap back?", [(' '.join([str(x), "Min"]), x) for x in range(1,10)])
+			self.session.openWithCallback(
+				self.confirmStart,
+				ChoiceBox,
+				"When to Zap back?",
+				[
+				 	(
+						' '.join([str(x), "Min"]),
+						x
+					)
+				 	for x in range(1, 10)
+				 ]
+			)
 
 	def confirmStop(self, result):
 		if result:
@@ -34,10 +58,15 @@ class WerbeZapper:
 			self.zap_service = None
 			self.move_service = None
 
-	def conditionalConfirm(self, result):
+	def confirmStart(self, result):
 		if result is not None:
 			# Show Information to User, Save Service, Start Timer
-			self.session.open(MessageBox, ' '.join(["Zapping back in", str(result[1]), "Minutes"]), type = MessageBox.TYPE_INFO, timeout = 3)
+			self.session.open(
+				MessageBox,
+				' '.join(["Zapping back in", str(result[1]), "Minutes"]),
+				type = MessageBox.TYPE_INFO,
+				timeout = 3
+			)
 
 			# Keep playing and selected service as we might be playing a service not in servicelist
 			self.zap_service = self.session.nav.getCurrentlyPlayingServiceReference()
@@ -64,13 +93,13 @@ class WerbeZapper:
 
 zapperInstance = None
 
+# Mainfunction
 def main(session, servicelist, **kwargs):
 	# Create Instance if none present, show Dialog afterwards
-	# TODO: do we really need to keep this instance?
 	global zapperInstance
 	if zapperInstance is None:
 		zapperInstance = WerbeZapper(session, servicelist)
 	zapperInstance.showSelection()
 
 def Plugins(**kwargs):
- 	return [ PluginDescriptor(name="Werbezapper", description="Automatically zaps back to current service after given Time", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main ) ]
+ 	return [PluginDescriptor(name="Werbezapper", description="Automatically zaps back to current service after given Time", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main)]
