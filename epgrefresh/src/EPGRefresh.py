a65176771d126d7efab818cb1bdad2d77d1ae866
ritzmo <ritzmo@a295cab0-163a-0410-857a-8207b47ee9d6>
2007-10-12 16:42:54 +0000

    EPGRefresh: Fix Plugin to actually work (a little) - uses PictureInPicture as target right now as I did not yet manage to write a functional FakeTarget (current progress checked in now too)
    git-svn-id: svn://svn.freaque.net/enigma2@127 a295cab0-163a-0410-857a-8207b47ee9d6

--- epgrefresh/src/EPGRefresh.py
+++ epgrefresh/src/EPGRefresh.py
@@ -1,10 +1,13 @@
-from enigma import eServiceCenter, getBestPlayableServiceReference, eServiceReference, eTimer
+# WIP Target
+#from FakeTarget import FakeTarget
 
-# Some target which is used nowhere else
-UNUSED_TARGET = 2
+# Testing
+from Screens.PictureInPicture import PictureInPicture
+
+from enigma import eTimer, eServiceReference
 
 # Duration to reside on service in s (to be configurable)
-DURATION = 120
+DURATION = 12
 
 class EPGRefresh:
     """WIP - Simple Class to refresh EPGData - WIP"""
@@ -13,22 +16,35 @@ class EPGRefresh:
         self.timer = eTimer()
         self.timer.timeout.get().append(self.nextService)
 
+        # Initialize Fake Target
+        self.target = None
+
         # Initialize
         self.position = -1
         # TODO: make this dynamic
         self.services = [
-            "1:0:1:6DCA:44D:1:C00000:0:0:0:",
-            "1:0:1:6D66:437:1:C00000:0:0:0:",
-            "1:0:1:445C:453:1:C00000:0:0:0:",
-            "1:0:1:2EE3:441:1:C00000:0:0:0:"
-        ] # List of ServiceRefs
+            eServiceReference(x)
+              for x in [
+                  "1:0:1:6DCA:44D:1:C00000:0:0:0:",
+                  "1:0:1:6D66:437:1:C00000:0:0:0:",
+                  "1:0:1:445C:453:1:C00000:0:0:0:",
+                  "1:0:1:2EE3:441:1:C00000:0:0:0:"
+              ]
+        ]
 
-    def refresh(self):
+    def refresh(self, session = None):
+        if self.target is None:
+            if session is not None:
+                self.target = session.instantiateDialog(PictureInPicture)
+                self.target.show()
+            else:
+                return
+            
         # Reset Position
         self.position = -1
 
-        # Start polling in 10ms
-        self.timer.start(10, True)
+        # Start polling in 100ms
+        self.timer.start(100, True)
 
     def nextService(self):
         # Increment Position
@@ -41,36 +57,16 @@ class EPGRefresh:
         # TODO: cache length?!
         if len(self.services) > self.position:
             # Play next service
-            self.playService(self.services[self.position])
-
-            # Start Timer
-            self.timer.startLongTimer(DURATION)
-
-        # Just timeout otherwise
-
-    # Modified Version of Screens.PictureInPicture.playService
-    def playService(self, service):
-        if service and (service.flags & eServiceReference.isGroup):
-            ref = getBestPlayableServiceReference(service, eServiceReference())
-        else:
-            ref = service
-
-        if ref:
-            # TODO: do we need to catch tune failed explicitly?
-            self.epgservice = eServiceCenter.getInstance().play(ref)
-            if self.epgservice and not self.pipservice.setTarget(UNUSED_TARGET):
-                # DEBUG
-                print "POLLING SERVICE", ref
-                self.epgservice.start()
+            if self.target.playService(self.services[self.position]):
+                # Start Timer
+                self.timer.startLongTimer(DURATION)
             else:
-                # DEBUG
-                print "COULD NOT POLL SERVICE", ref
-
-                # Destroy
-                self.epgservice = None
-
-                # Poll next service
-                self.timer.stop()
+                # Skip service if play failed
                 self.nextService()
+        else:
+            # Destroy service
+            self.target.hide()
+            #self.target.stop()
+            pass
 
 epgrefresh = EPGRefresh()
--- epgrefresh/src/plugin.py
+++ epgrefresh/src/plugin.py
@@ -5,6 +5,9 @@
 # but unfortunately I'm unable to zap non-mainwindow from outside
 # enigma2 and eTimer's can't be run with dynamic length
 #
+# Right now it used PictureInPicture as target so you will
+# notice when it's active and it WILL override the active channel
+#
 
 # Plugin
 from EPGRefresh import epgrefresh
@@ -14,7 +17,7 @@ from Plugins.Plugin import PluginDescriptor
 
 # Mainfunction
 def main(session, **kwargs):
-	epgrefresh.refresh()
+	epgrefresh.refresh(session)
 
 def Plugins(**kwargs):
 	return [
