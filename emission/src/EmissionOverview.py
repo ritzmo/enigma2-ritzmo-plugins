38ee720ca85fe3a7c78da6c7cdf51a20e37e1cb9
ritzmo <ritzmo@a295cab0-163a-0410-857a-8207b47ee9d6>
2009-03-09 02:34:54 +0000

    enigma2-plugins/emission: different list types (all/dl/ul)
    git-svn-id: svn://svn.freaque.net/enigma2@704 a295cab0-163a-0410-857a-8207b47ee9d6

--- emission/src/EmissionOverview.py
+++ emission/src/EmissionOverview.py
@@ -4,6 +4,8 @@ from Screens.Screen import Screen
 from Components.MenuList import MenuList
 from Components.ActionMap import ActionMap
 from Components.Button import Button
+from Components.Label import Label
+from Components.Pixmap import Pixmap
 
 from Components.config import config
 
@@ -38,17 +40,27 @@ class EmissionOverviewList(MenuList):
 		]
 
 class EmissionOverview(Screen):
-	skin = """<screen name="EmissionOverview" title="Torrent Overview" position="75,155" size="565,280">
-		<widget name="list" position="5,5" size="555,225" scrollbarMode="showOnDemand" />
-		<ePixmap position="0,235" zPosition="4" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
-		<ePixmap position="140,235" zPosition="4" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
-		<ePixmap position="280,235" zPosition="4" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
-		<ePixmap position="420,235" zPosition="4" size="140,40" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
-		<widget name="key_red" position="0,235" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget name="key_green" position="140,235" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget name="key_yellow" position="280,235" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget name="key_blue" position="420,235" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+	skin = """<screen name="EmissionOverview" title="Torrent Overview" position="75,155" size="565,300">
+		<widget size="320,25" alphatest="on" position="5,5" zPosition="1" name="all_sel" pixmap="skin_default/epg_now.png" />
+		<widget valign="center" transparent="1" size="108,22" backgroundColor="#25062748" position="5,10" zPosition="2" name="all_text" halign="center" font="Regular;18" />
+		<widget size="320,25" alphatest="on" position="5,5" zPosition="1" name="downloading_sel" pixmap="skin_default/epg_next.png" />
+		<widget valign="center" transparent="1" size="108,22" backgroundColor="#25062748" position="111,10" zPosition="2" name="downloading_text" halign="center" font="Regular;18" />
+		<widget size="320,25" alphatest="on" position="5,5" zPosition="1" name="seeding_sel" pixmap="skin_default/epg_more.png" />
+		<widget valign="center" transparent="1" size="108,22" backgroundColor="#25062748" position="200,10" zPosition="2" name="seeding_text" halign="center" font="Regular;18" />
+		<widget name="list" position="5,30" size="555,225" scrollbarMode="showOnDemand" />
+		<ePixmap position="0,260" zPosition="4" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
+		<ePixmap position="140,260" zPosition="4" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
+		<ePixmap position="280,260" zPosition="4" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
+		<ePixmap position="420,260" zPosition="4" size="140,40" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
+		<widget name="key_red" position="0,260" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+		<widget name="key_green" position="140,260" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+		<widget name="key_yellow" position="280,260" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
+		<widget name="key_blue" position="420,260" zPosition="5" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
 	</screen>"""
+
+	LIST_TYPE_ALL = 0
+	LIST_TYPE_DOWNLOADING = 1
+	LIST_TYPE_SEEDING = 2
 
 	def __init__(self, session):
 		Screen.__init__(self, session)
@@ -67,21 +79,76 @@ class EmissionOverview(Screen):
 
 		self["ColorActions"] = ActionMap(["ColorActions"],
 		{
-			"blue": self.configure,
+			"green": self.configure,
+			"yellow": self.prevlist,
+			"blue": self.nextlist,
 		})
 
 		self["key_red"] = Button(_("Cancel"))
-		self["key_green"] = Button("")
-		self["key_yellow"] = Button("")
-		self["key_blue"] = Button(_("Configure"))
+		self["key_green"] = Button(_("Configure"))
+		self["key_yellow"] = Button(_("Seeding"))
+		self["key_blue"] = Button(_("Download"))
 
-		# note:
-		# This should give some kind of overview actually (currently only name)
-		# maybe differents types of lists (unrelated) like in original transmission client
+		self["all_text"] = Label(_("All"))
+		self["downloading_text"] = Label(_("DL"))
+		self["seeding_text"] = Label(_("UL"))
+
+		self.list_type = self.LIST_TYPE_ALL
+
+		self["all_sel"] = Pixmap()
+		self["downloading_sel"] = Pixmap()
+		self["downloading_sel"].hide()
+		self["seeding_sel"] = Pixmap()
+		self["seeding_sel"].hide()
+
+		# This should actually give some kind of overview (currently just name and eta or is this enough?)
 		self['list'] = EmissionOverviewList([])
+
 		self.timer = eTimer()
 		self.timer.callback.append(self.updateList)
 		self.timer.start(0, 1)
+
+	def showHideSetTextMagic(self):
+		if self.list_type == self.LIST_TYPE_ALL:
+			self["all_sel"].show()
+			self["downloading_sel"].hide()
+			self["seeding_sel"].hide()
+			self["key_yellow"].setText(_("Seeding"))
+			self["key_blue"].setText(_("Download"))
+		elif self.list_type == self.LIST_TYPE_DOWNLOADING:
+			self["all_sel"].hide()
+			self["downloading_sel"].show()
+			self["seeding_sel"].hide()
+			self["key_yellow"].setText(_("All"))
+			self["key_blue"].setText(_("Seeding"))
+		elif self.list_type == self.LIST_TYPE_SEEDING:
+			self["all_sel"].hide()
+			self["downloading_sel"].hide()
+			self["seeding_sel"].show()
+			self["key_yellow"].setText(_("Download"))
+			self["key_blue"].setText(_("Seeding"))
+
+	def prevlist(self):
+		self.timer.stop()
+		if self.list_type == self.LIST_TYPE_ALL:
+			self.list_type = self.LIST_TYPE_SEEDING
+		elif self.list_type == self.LIST_TYPE_DOWNLOADING:
+			self.list_type = self.LIST_TYPE_ALL
+		elif self.list_type == self.LIST_TYPE_SEEDING:
+			self.list_type = self.LIST_TYPE_DOWNLOADING
+		self.showHideSetTextMagic()
+		self.updateList()
+
+	def nextlist(self):
+		self.timer.stop()
+		if self.list_type == self.LIST_TYPE_ALL:
+			self.list_type = self.LIST_TYPE_DOWNLOADING
+		elif self.list_type == self.LIST_TYPE_DOWNLOADING:
+			self.list_type = self.LIST_TYPE_SEEDING
+		elif self.list_type == self.LIST_TYPE_SEEDING:
+			self.list_type = self.LIST_TYPE_ALL
+		self.showHideSetTextMagic()
+		self.updateList()
 
 	def configureCallback(self):
 		self.transmission = transmission.Client(
@@ -105,7 +172,13 @@ class EmissionOverview(Screen):
 			list = self.transmission.list().values()
 		except transmission.TransmissionError:
 			list = []
-		self.list = [(x,) for x in list]
+		if self.list_type == self.LIST_TYPE_ALL:
+			self.list = [(x,) for x in list]
+		elif self.list_type == self.LIST_TYPE_DOWNLOADING:
+			self.list = [(x,) for x in list if x.status == "downloading"]
+		elif self.list_type == self.LIST_TYPE_SEEDING:
+			self.list = [(x,) for x in list if x.status == "seeding"]
+
 		self['list'].setList(self.list)
 		self.timer.startLongTimer(10)
 
