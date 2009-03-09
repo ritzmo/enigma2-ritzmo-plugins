d09f0df8d4ba43e50003df3fb4fcd1c2e0fcb3a7
ritzmo <ritzmo@a295cab0-163a-0410-857a-8207b47ee9d6>
2009-03-09 22:25:22 +0000

    enigma2-plugins/emission: allow to download torrent enclosures from simplerss (yay, now you can use this as autodownloader :P), add .tor/.torrent mimetypes
    git-svn-id: svn://svn.freaque.net/enigma2@709 a295cab0-163a-0410-857a-8207b47ee9d6

--- emission/src/plugin.py
+++ emission/src/plugin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from Plugins.Plugin import PluginDescriptor
-from Components.config import config, ConfigSubsection, ConfigText, ConfigNumber
+from Components.config import config, ConfigSubsection, ConfigText, \
+	ConfigNumber, ConfigYesNo
 import EmissionOverview
 
 from transmission import transmission
@@ -11,6 +12,43 @@ config.plugins.emission.hostname = ConfigText(default = "localhost", fixed_size 
 config.plugins.emission.username = ConfigText(default = "", fixed_size = False)
 config.plugins.emission.password = ConfigText(default = "", fixed_size = False)
 config.plugins.emission.port = ConfigNumber(default = 9091)
+config.plugins.emission.autodownload_from_simplerss = ConfigYesNo(default = False)
+
+def simplerss_update_callback(id = None):
+	try:
+		from Plugins.Extensions.SimpleRSS.plugin import rssPoller
+	except ImportError:
+		pass # should not happen since the poller has to be active for us to be called :-)
+	else:
+		# we only check the "new items" feed currently since we do not keep track of the files we downloaded
+		if id is None:
+			client = None
+			for item in rssPoller.newItemFeed.history:
+				for file in item[3]:
+					if file.mimetype == "application/x-bittorrent":
+						if client is None:
+							client = transmission.Client(
+								address = config.plugins.emission.hostname.value,
+								port = config.plugins.emission.port.value,
+								user = config.plugins.emission.username.value,
+								password = config.plugins.emission.password.value
+							)
+						# XXX: we might want to run this in the background cause this might block...
+						client.add_url(file.path)
+
+def simplerss_handle_callback(el):
+	try:
+		from Plugins.Extensions.SimpleRSS.RSSPoller import update_callbacks
+	except ImportError:
+		# XXX: we might want to handle this better than just ignoring it
+		pass
+	else:
+		if el.value and simplerss_update_callback not in update_callbacks:
+			update_callbacks.append(simplerss_update_callback)
+		elif not el.value and simplerss_update_callback in update_callbacks:
+			update_callbacks.remove(simplerss_update_callback)
+
+config.plugins.emission.autodownload_from_simplerss.addNotifier(simplerss_handle_callback, immediate_feedback = False)
 
 def main(session, **kwargs):
 	reload(EmissionOverview)
@@ -46,6 +84,10 @@ def filescan_open(item, session, **kwargs):
 		type = MessageBox.TYPE_INFO,
 		timeout = 5
 	)
+
+from mimetypes import add_type
+add_type("application/x-bittorrent", ".tor")
+add_type("application/x-bittorrent", ".torrent")
 
 def filescan(**kwargs):
 	from Components.Scanner import Scanner, ScanPath
